fffd5ce84ded93a70fd9e84072d34087d38920a8
Trisha Kholiya <tktrishakholiya@gmail.com>
2025-12-08 15:53:56 -0800

    Added more benchmarking functions

--- benchmarking/benchmark_multiplication.cpp
+++ benchmarking/benchmark_multiplication.cpp
@@ -2,33 +2,33 @@
 #include "matrix.h"
 #include <armadillo>
 
-static void BM_MatrixAddition_MatrixClass(benchmark::State& state) {
+static void BM_MatrixMultiplication_MatrixClass(benchmark::State& state) {
     int n = state.range(0);
     Matrix A = Matrix::Random(n, n);
     Matrix B = Matrix::Random(n, n);
 
     for (auto _ : state) {
-        benchmark::DoNotOptimize(A + B);
+        benchmark::DoNotOptimize(A * B);
     }
 
     state.SetItemsProcessed(state.iterations() * n * n);
 }
 
-static void BM_MatrixAddition_Armadillo(benchmark::State& state) {
+static void BM_MatrixMultiplication_Armadillo(benchmark::State& state) {
     int n = state.range(0);
     arma::mat A = arma::randu<arma::mat>(n, n);
     arma::mat B = arma::randu<arma::mat>(n, n);
 
     for (auto _ : state) {
-      arma::mat C = A + B;
+      arma::mat C = A * B;
       benchmark::DoNotOptimize(C.memptr()); // needs to be pointer to memory NOT arma object
     }
 
     state.SetItemsProcessed(state.iterations() * n * n);
 }
 
-// benchmark for differenct sizes
-BENCHMARK(BM_MatrixAddition_MatrixClass)
+// Register benchmarks for different sizes
+BENCHMARK(BM_MatrixMultiplication_MatrixClass)
   ->Arg(4)
   ->Arg(10)
   ->Arg(100)
@@ -36,7 +36,7 @@ BENCHMARK(BM_MatrixAddition_MatrixClass)
   ->Arg(400)
   ->Arg(800);
 
-BENCHMARK(BM_MatrixAddition_Armadillo)
+BENCHMARK(BM_MatrixMultiplication_Armadillo)
   ->Arg(4)
   ->Arg(10)
   ->Arg(100)
