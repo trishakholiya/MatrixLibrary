3da88c0f064192ebd78c9218165968199aa2eb01
Trisha Kholiya <tktrishakholiya@gmail.com>
2025-12-09 18:04:47 -0800

    Updated the print function to match armadillo print

--- include/print_vec.hpp
+++ include/print_vec.hpp
@@ -3,10 +3,8 @@
 #include <vector>
 
 inline std::ostream& operator<<(std::ostream& out, const std::vector<double>& v) {
-  out << "[ ";
   for (double x : v) {
-    out << x << " ";
+    out << x << "\n";
   }
-  out << "]";
   return out;
 }
